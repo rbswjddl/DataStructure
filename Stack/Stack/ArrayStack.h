d47883735f980060187e4758fb91aac7ebc245a9
JeongGyun <dlwjdrbs12@naver.com>
2021-05-17 10:58:24 +0900

    Make ArrayStack

---
+++ Stack/Stack/ArrayStack.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define STACK_LEN	100
+#define TRUE		1
+#define FALSE		0
+
+typedef int			Data;
+
+typedef struct
+{
+	Data stack[100];
+
+	int top;
+
+}ArrayStack;
+
+typedef ArrayStack	Stack;
+
+void StackInit(Stack* pStack);
+int SIsEmpty(Stack* pStack);
+
+int SPush(Stack* pStack, Data pData);
+int SPop(Stack* pStack, Data* pData);
+int SPeek(Stack* pStack, Data* pData);
---
+++ Stack/Stack/ArrayStackMain.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "ArrayStack.h"
+
+int main()
+{
+	Stack stack;
+	int iData;
+
+	StackInit(&stack);
+
+	for (int iCount = 0; iCount < 100; iCount++)
+	{
+		if (SPush(&stack, iCount) == FALSE)
+		{
+			printf("Fail to Push\n");
+			return;
+		}
+	}
+	
+	while (!SIsEmpty(&stack))
+	{
+		if (SPop(&stack, &iData) == FALSE)
+		{
+			printf("Fail to Pop\n");
+			return;
+		}
+		printf("%d ", iData);
+	}
+
+	return 0;
+}
