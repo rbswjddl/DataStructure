64e071cd8f1b8dab87292732f6e8c9f6b22a3b8b
JeongGyun <dlwjdrbs12@naver.com>
2021-05-18 10:30:33 +0900

    중위 표기법을 후위 표기법으로

--- Stack/Stack/ListStackMain.c
+++ Stack/Stack/ListStackMain.c
@@ -1,32 +1,95 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
 #include "ListStack.h"
 
-int main()
+int GetOpPrec(char op)
+{
+	switch (op)
+	{
+	case '(': return 1;
+	case '+': case '-': return 3;
+	case '*': case '/': return 5;
+	}
+
+	return -1;
+}
+
+int WhoOpPrec(char op1, char op2)
+{
+	int iOpPrec1 = GetOpPrec(op1);
+	int iOpPrec2 = GetOpPrec(op2);
+
+	return iOpPrec1 - iOpPrec2;
+}
+
+void ConvToRPNExp(char exp[])
 {
 	Stack stack;
-	int iData;
+	int iStrLen = strlen(exp);
+	char* pTempStr = (char*)malloc(iStrLen + 1);
+
+	int iTempIdx = 0;
+	int iToken, iData;
 
 	StackInit(&stack);
 
-	for (int iCount = 0; iCount < 100; iCount++)
+	for (int idx = 0; idx < iStrLen; idx++)
 	{
-		if (SPush(&stack, iCount) == FALSE)
+		iToken = exp[idx];
+
+		if (isdigit(iToken))
 		{
-			printf("Fail to Push\n");
-			return;
+			pTempStr[iTempIdx++] = iToken;
+		}
+		else
+		{
+			switch (iToken)
+			{
+			case '(':
+				SPush(&stack, iToken);
+				break;
+			case ')':
+				while (1)
+				{
+					SPop(&stack, &iData);
+					if (iData == '(') break;
+					
+					pTempStr[iTempIdx++] = iData;
+				}
+				break;
+			case '+': case '-':
+			case '*': case '/':
+				while (!SIsEmpty(&stack))
+				{
+					SPeek(&stack, &iData);
+					if (WhoOpPrec(iData, iToken) < 0) break;
+
+					SPop(&stack, &iData);
+					pTempStr[iTempIdx++] = iData;
+				}
+				SPush(&stack, iToken);
+			}
 		}
 	}
 
 	while (!SIsEmpty(&stack))
 	{
-		if (SPop(&stack, &iData) == FALSE)
-		{
-			printf("Fail to Pop\n");
-			return;
-		}
-		printf("%d ", iData);
+		SPop(&stack, &iData);
+		pTempStr[iTempIdx++] = iData;
 	}
 
-	return 0;
+	pTempStr[iTempIdx++] = NULL;
+	memcpy(exp, pTempStr, iTempIdx);
+	free(pTempStr);
+}
+
+int main()
+{
+	char exp[] = "1+2*4*(1-2)";
+
+	ConvToRPNExp(exp);
+
+	printf("%s\n", exp);
 }
