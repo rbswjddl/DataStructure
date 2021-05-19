ec7f60ca896305ea594d0f94a68c6f4002ee3489
JeongGyun <dlwjdrbs12@naver.com>
2021-05-19 09:54:16 +0900

    ignore

--- Stack/Stack/ArrayStack.c
+++ Stack/Stack/ArrayStack.c
@@ -1,44 +1,44 @@
-#include "ArrayStack.h"
-
-void StackInit(Stack* pStack)
-{
-	pStack->top = -1;
-}
-
-int SIsEmpty(Stack* pStack)
-{
-	if (pStack->top == -1)
-		return TRUE;
-	
-	return FALSE;
-}
-
-int SPush(Stack* pStack, Data data)
-{
-	if (pStack->top + 1 >= STACK_LEN)
-		return FALSE;
-
-	pStack->top++;
-	pStack->stack[pStack->top] = data;
-
-	return TRUE;
-}
-
-int SPop(Stack* pStack, Data* pData)
-{
-	if (SIsEmpty(pStack))
-		return FALSE;
-
-	*pData = pStack->stack[pStack->top];
-	pStack->top--;
-	return TRUE;
-}
-
-int SPeek(Stack* pStack, Data* pData)
-{
-	if (pStack->top == -1)
-		return FALSE;
-
-	*pData = pStack->stack[pStack->top];
-	return TRUE;
-}
+//#include "ArrayStack.h"
+//
+//void StackInit(Stack* pStack)
+//{
+//	pStack->top = -1;
+//}
+//
+//int SIsEmpty(Stack* pStack)
+//{
+//	if (pStack->top == -1)
+//		return TRUE;
+//	
+//	return FALSE;
+//}
+//
+//int SPush(Stack* pStack, Data data)
+//{
+//	if (pStack->top + 1 >= STACK_LEN)
+//		return FALSE;
+//
+//	pStack->top++;
+//	pStack->stack[pStack->top] = data;
+//
+//	return TRUE;
+//}
+//
+//int SPop(Stack* pStack, Data* pData)
+//{
+//	if (SIsEmpty(pStack))
+//		return FALSE;
+//
+//	*pData = pStack->stack[pStack->top];
+//	pStack->top--;
+//	return TRUE;
+//}
+//
+//int SPeek(Stack* pStack, Data* pData)
+//{
+//	if (pStack->top == -1)
+//		return FALSE;
+//
+//	*pData = pStack->stack[pStack->top];
+//	return TRUE;
+//}
--- Stack/Stack/ListStackMain.c
+++ Stack/Stack/ListStackMain.c
@@ -1,89 +1,6 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
-#include "ListStack.h"
-
-int GetOpPrec(char op)
-{
-	switch (op)
-	{
-	case '(': return 1;
-	case '+': case '-': return 3;
-	case '*': case '/': return 5;
-	}
-
-	return -1;
-}
-
-int WhoOpPrec(char op1, char op2)
-{
-	int iOpPrec1 = GetOpPrec(op1);
-	int iOpPrec2 = GetOpPrec(op2);
-
-	return iOpPrec1 - iOpPrec2;
-}
-
-void ConvToRPNExp(char exp[])
-{
-	Stack stack;
-	int iStrLen = strlen(exp);
-	char* pTempStr = (char*)malloc(iStrLen + 1);
-
-	int iTempIdx = 0;
-	int iToken, iData;
-
-	StackInit(&stack);
-
-	for (int idx = 0; idx < iStrLen; idx++)
-	{
-		iToken = exp[idx];
-
-		if (isdigit(iToken))
-		{
-			pTempStr[iTempIdx++] = iToken;
-		}
-		else
-		{
-			switch (iToken)
-			{
-			case '(':
-				SPush(&stack, iToken);
-				break;
-			case ')':
-				while (1)
-				{
-					SPop(&stack, &iData);
-					if (iData == '(') break;
-					
-					pTempStr[iTempIdx++] = iData;
-				}
-				break;
-			case '+': case '-':
-			case '*': case '/':
-				while (!SIsEmpty(&stack))
-				{
-					SPeek(&stack, &iData);
-					if (WhoOpPrec(iData, iToken) < 0) break;
-
-					SPop(&stack, &iData);
-					pTempStr[iTempIdx++] = iData;
-				}
-				SPush(&stack, iToken);
-			}
-		}
-	}
-
-	while (!SIsEmpty(&stack))
-	{
-		SPop(&stack, &iData);
-		pTempStr[iTempIdx++] = iData;
-	}
-
-	pTempStr[iTempIdx++] = NULL;
-	memcpy(exp, pTempStr, iTempIdx);
-	free(pTempStr);
-}
+#include "InfixToPostfix.h"
+#include "PostCalculator.h"
 
 int main()
 {
@@ -91,5 +8,6 @@ int main()
 
 	ConvToRPNExp(exp);
 
-	printf("%s\n", exp);
+	int result = EvalRPNExp(exp);
+	printf("%d\n", result);
 }
